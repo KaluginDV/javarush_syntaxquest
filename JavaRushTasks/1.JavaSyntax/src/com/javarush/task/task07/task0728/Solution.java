f1baaa0c5b322b6c642ab9e0b8c6bdcfdffe48f0
KaluginDV <interrupted-man@yandex.ru>
2017-08-31 23:46:24 +0800

    Решена задача 07-28

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task07/task0728/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task07.task0728;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+/* 
+В убывающем порядке
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] array = new int[20];
+        for (int i = 0; i < 20; i++) {
+            array[i] = Integer.parseInt(reader.readLine());
+        }
+
+        sort(array);
+
+        for (int x : array) {
+            System.out.println(x);
+        }
+    }
+
+    public static void sort(int[] array) {
+        //напишите тут ваш код
+        Integer[] newArray = new Integer[20];
+        for (int i = 0; i<array.length; i++) {
+            newArray[i] = array[i];
+        }
+        Arrays.sort(newArray, Collections.reverseOrder());
+        for (int i = 0; i<array.length; i++) {
+            array[i] = newArray[i];
+        }
+    }
+}
